42b572e47c5c1a3960b46180b39b773956f41c6e
JeanPiton <jean.orpiton@gmail.com>
2023-05-03 21:42:54 -0300

    Suggestions 2.0

--- src/Suggestion.js
+++ src/Suggestion.js
@@ -1,30 +1,15 @@
-export default function Suggestion() {
-    const itens = [
-        {src:"assets/img/bad.vibes.memes.svg",alt:"bad.vibes.memes.svg",nome:'bad.vibes.memes',razao:'Segue você'}
-        ,
-        {src:"assets/img/chibirdart.svg",alt:"chibirdart",nome:"chibirdart",razao:"Segue você"}
-        ,
-        {src:"assets/img/razoesparaacreditar.svg",alt:"razoesparaacreditar",nome:"razoesparaacreditar",razao:"Novo no Instagram"}
-        ,
-        {src:"assets/img/adorable_animals.svg",alt:"adorable_animals",nome:"adorable_animals",razao:"Segue você"}
-        ,
-        {src:"assets/img/smallcutecats.svg",alt:"smallcutecats",nome:"smallcutecats",razao:"Segue você"}
-    ]
+export default function Suggestion(props) {
     return (
-        <>
-            {itens.map((item) =>
-                <div class="sugestao">
-                    <div class="usuario">
-                        <img src={item.src} alt={item.alt} />
-                        <div class="texto">
-                            <div class="nome">{item.nome}</div>
-                            <div class="razao">{item.razao}</div>
-                        </div>
-                    </div>
-
-                    <div class="seguir">Seguir</div>
+        <div class="sugestao">
+            <div class="usuario">
+                <img src={props.src} alt={props.alt} />
+                <div class="texto">
+                    <div class="nome">{props.nome}</div>
+                    <div class="razao">{props.razao}</div>
                 </div>
-            )}
-        </>
+            </div>
+
+            <div class="seguir">Seguir</div>
+        </div>
     );
 }
--- src/Suggestions.js
+++ src/Suggestions.js
@@ -1,12 +1,23 @@
 import Suggestion from "./Suggestion";
 export default function Suggestions() {
+    const itens = [
+        {src:"assets/img/bad.vibes.memes.svg",alt:"bad.vibes.memes.svg",nome:'bad.vibes.memes',razao:'Segue você'}
+        ,
+        {src:"assets/img/chibirdart.svg",alt:"chibirdart",nome:"chibirdart",razao:"Segue você"}
+        ,
+        {src:"assets/img/razoesparaacreditar.svg",alt:"razoesparaacreditar",nome:"razoesparaacreditar",razao:"Novo no Instagram"}
+        ,
+        {src:"assets/img/adorable_animals.svg",alt:"adorable_animals",nome:"adorable_animals",razao:"Segue você"}
+        ,
+        {src:"assets/img/smallcutecats.svg",alt:"smallcutecats",nome:"smallcutecats",razao:"Segue você"}
+    ]
     return (
         <div class="sugestoes">
             <div class="titulo">
                 Sugestões para você
                 <div>Ver tudo</div>
             </div>
-            <Suggestion />
+            {itens.map(item => <Suggestion src={item.src} alt={item.alt} nome={item.nome} razao={item.razao} />)}
         </div>
     );
 }
